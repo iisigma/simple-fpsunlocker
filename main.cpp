8660a6bede043f8ba81f455a95e92d451a49e0b0
ファック・ユー <52034926+iisigma@users.noreply.github.com>
2022-04-04 13:49:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 static const auto current_module = reinterpret_cast<std::uintptr_t>(GetModuleHandleA(NULL));
 template<typename T>
-T rebase(const std::uintptr_t& address) {
+const T rebase(const std::uintptr_t& address) {
 	return reinterpret_cast<T>(current_module + address);
 }
 
@@ -17,37 +17,37 @@ bool is_numeric(const std::string& str) {
 	return true;
 }
 
-void init() {
-	DWORD old{};
-	FILE* stream{};
-
-	VirtualProtect(reinterpret_cast<void*>(&FreeConsole), 1, PAGE_EXECUTE_READWRITE, &old);
-	*reinterpret_cast<std::uint8_t*>(&FreeConsole) = 0xC3;
-	VirtualProtect(reinterpret_cast<void*>(&FreeConsole), 1, old, &old);
-
-	AllocConsole();
-	SetConsoleTitleA("simple fps unlocker");
-	freopen_s(&stream, "conin$", "r", stdin);
-	freopen_s(&stream, "conout$", "w", stdout);
-
-	static const auto getscheduler = rebase<std::uintptr_t(__cdecl*)()>(0xDCD950);
-	std::cout << getscheduler() << std::endl;
-
-	for (;;) {
-		reentry:
-		std::string val{};
-		std::getline(std::cin, val);
-
-		if (!is_numeric(val))
-			goto reentry;
-
-		*reinterpret_cast<double*>(getscheduler() + 0x118) = 1 / std::stod(val);
-		std::cout << std::stod(val) << std::endl;
-	}
-}
-
 std::uint8_t WINAPI DllMain(int, int type, int) {
-	if (type == DLL_PROCESS_ATTACH)
-		std::thread{ init }.detach();
+	if (type == DLL_PROCESS_ATTACH) {
+		std::thread([]() -> void {
+			DWORD old{};
+			FILE* stream{};
+
+			VirtualProtect(reinterpret_cast<void*>(&FreeConsole), 1, PAGE_EXECUTE_READWRITE, &old);
+			*reinterpret_cast<std::uint8_t*>(&FreeConsole) = 0xC3;
+			VirtualProtect(reinterpret_cast<void*>(&FreeConsole), 1, old, &old);
+
+			AllocConsole();
+			SetConsoleTitleA("simple fps unlocker");
+			freopen_s(&stream, "conin$", "r", stdin);
+			freopen_s(&stream, "conout$", "w", stdout);
+
+			static const auto getscheduler = rebase<std::uintptr_t(__cdecl*)()>(0xDCD950);
+			std::cout << getscheduler() << std::endl;
+
+			for (;;) {
+			reentry:
+				std::string val{};
+				std::getline(std::cin, val);
+
+				if (!is_numeric(val))
+					goto reentry;
+
+				*reinterpret_cast<double*>(getscheduler() + 0x118) = 1 / std::stod(val);
+				std::cout << std::stod(val) << std::endl;
+				std::cout << *reinterpret_cast<double*>(getscheduler() + 0x118) << std::endl;
+			}
+		}).detach();
+	}
 	return TRUE;
 }
